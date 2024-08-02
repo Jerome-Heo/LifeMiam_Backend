8747966f1c96f2daa37c4e6262a50b5b04558936
ThierryPa <papouchado.thierry@gmail.com>
2024-08-02 19:18:11 +0200

    Premiere grosse structure du projet backend: express generator / dotenv / cors / uid / bcrypt / mongoose / connection à la DB / module: checkBody

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+const User = require('../models/users');
+const { checkBody } = require('../modules/checkBody');
+
+// UID2
+const uid2 = require('uid2');
+// A redéfinir plus tard SERT d'exemple
+const token = uid2(32);
+
+// BCrypt
+const bcrypt = require('bcrypt');
+// A redéfinir plus tard SERT d'exemple
+const hash = bcrypt.hashSync('password', 10);
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
