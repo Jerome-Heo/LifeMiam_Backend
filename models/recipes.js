984277848650e0f3e84314d666f9820e5d4c329a
ThierryPa <papouchado.thierry@gmail.com>
2024-08-05 12:12:59 +0200

    Fixed Models/Recipes / Models/Ingredients and Added Routes/Menus and Routes/Shop

--- models/recipes.js
+++ models/recipes.js
@@ -2,16 +2,18 @@ const mongoose = require('mongoose');
 
 const recipesSchema = mongoose.Schema({
     name: String,
-    tags: Array[String],
-    regime: Array[String],
+    tags: [String],
+    regime: [String],
     image: String,
     default_serving: Number,
-    ing: Array[Object, Number],
-    steps: Array[String],
-    difficulty: Enumerator,
+    ing: String,
+    steps: [String],
+    difficulty: {type : String, enum: ['très facile', 'facile', 'moyen', 'dur']},
     time: Number,
     popularity: Number,
 })
+
+//const ingredients
 
 const Recipe = mongoose.model('recipes', recipesSchema);
 
---
+++ routes/menus.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+const Menu = require('../models/menus');
+const { checkBody } = require('../modules/checkBody');
+
+const URL = "http://localhost:3000";
+
+/* GET recipes listing with parameters. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+module.exports = router;
