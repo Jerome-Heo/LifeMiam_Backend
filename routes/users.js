0b79bc5492d9201791ec346b4de31fb837499d03
ThierryPa <papouchado.thierry@gmail.com>
2024-08-05 14:39:56 +0200

    users routes updated: sign in / sign up

--- routes/users.js
+++ routes/users.js
@@ -14,9 +14,51 @@ const bcrypt = require('bcrypt');
 // A redéfinir plus tard SERT d'exemple
 const hash = bcrypt.hashSync('password', 10);
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+// Sign In for the user
+router.post('/signin', (req, res) => {
+  if (!checkBody(req.body, ['username', 'password' ])) {
+    res.json({ result: false, error: 'Missing or empty fields' });
+    return;
+  }
+
+  User.findOne({ username: req.body.username }).then(data => {
+    if (data && bcrypt.compareSync(req.body.password, data.password)) {
+      res.json({ result: true, token: data.token });
+    } else {
+      res.json({ result: false, error: 'User not found or wrong password' });
+    }
+  });
+});
+
+router.post('/signup', (req, res) => {
+  if (!checkBody(req.body, ['username', 'email', 'password' ])) {
+    res.json({ result: false, error: 'Missing or empty fields' });
+    return;
+  }
+
+  // Check if the user has not already been registered
+  User.findOne({ username: req.body.username }).then(data => {
+    if (data === null) {
+      const hash = bcrypt.hashSync(req.body.password, 10);
+
+      const newUser = new User({
+        username: req.body.username,
+        email: req.body.email,
+        password: hash,
+        token: uid2(32),
+        regime: req.body.regime,
+        signup_date: Date.now(),
+        menus: [],
+      });
+
+      newUser.save().then(newDoc => {
+        res.json({ result: true, token: newDoc.token });
+      });
+    } else {
+      // User already exists in database
+      res.json({ result: false, error: 'User already exists' });
+    }
+  });
 });
 
 module.exports = router;
