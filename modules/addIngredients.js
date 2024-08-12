1b3ffa40e9d024174f7a5458bb089378a0c5b7a9
patleserdev <leserpat@hotmail.com>
2024-08-12 12:58:43 +0200

    repair /modules/addingredient

--- modules/addIngredients.js
+++ modules/addIngredients.js
@@ -1,26 +1,28 @@
 function addIngredients(ingredients) {
     let count = [];
   
-  //   console.log(ingredients);
+  //  console.log('ingredientsbefortransform',ingredients);
     let countKeys = Object.keys(count);
   //   console.log(countKeys);
   
-    for (let ingredient of ingredients) {
+    for (let ingredient of ingredients) 
+    {
       const ingToFind = countKeys.find((e) => e === ingredient.name);
-      // console.log(countKeys);
+      // console.log('countkeys',countKeys);
       if (!ingToFind) 
       {
       let quantity=0
-      
+      // console.log('ingredient',ingredient);
       ingredients.filter((e) => e.name === ingredient.name ? quantity+= e.quantity : null );
-      //   console.log(listFiltered);
-        count.push({name: ingredient.name , quantity: quantity, unit: ingredient.unit, category:ingredient.category})
+      
+      count.push({name: ingredient.name , quantity: quantity, unit: ingredient.unit, category:ingredient.category})
+      countKeys.push(ingredient.name)
       //   count['unit'] = ingredient.unit
       //   console.log(ingredient.name);
       // console.log(quantity);
       }
     }
-
+ 
     return count
 }
 
